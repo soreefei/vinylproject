bdea3d9f437b61b5b394ca057c52e8381d722ebf
Evan Soreefan <evan.soreefan@siemens.com>
2025-09-22 16:14:38 -0400

    Deleted .jsx duplicates front-end

--- src/dashboard/components/SideMenu.tsx
+++ src/dashboard/components/SideMenu.tsx
@@ -68,16 +68,16 @@ export default function SideMenu() {
       >
         <Avatar
           sizes="small"
-          alt="Riley Carter"
+          alt="Evan Soreefan"
           src="/static/images/avatar/7.jpg"
           sx={{ width: 36, height: 36 }}
         />
         <Box sx={{ mr: 'auto' }}>
           <Typography variant="body2" sx={{ fontWeight: 500, lineHeight: '16px' }}>
-            Riley Carter
+            Evan Soreefan
           </Typography>
           <Typography variant="caption" sx={{ color: 'text.secondary' }}>
-            riley@email.com
+            evansoreefan123@icloud.com
           </Typography>
         </Box>
         <OptionsMenu />
